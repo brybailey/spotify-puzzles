0278c9de631bd26916ab663e27b663493666eb90
Bryan C. Bailey <bryanbailey@mac.com>
2017-11-16 22:27:30 +0100

    solved, time to commit

--- Reversebinary/reverseBinary.c
+++ Reversebinary/reverseBinary.c
@@ -1,4 +1,4 @@
-// Prime finder
+// Reverse binary 
 // (c) 2017 Bryan C. Bailey
 
 #include <stdio.h>
@@ -8,21 +8,18 @@ int main() {
   int in;
   scanf( "%d", &in );
 
-  int out;
+  int out=0;
   int test;
 
   while( in != 0 ) {
     test = in&1;
-    out = 1<<out;
+    out = out<<1;
     if( (test&1) == 1 ) {
       out++;
     }
-
-    in = in >> 1;
+    in = in>>1;
   }
-
-  printf( "%d is out\n", out );
-
+  printf( "%d", out );
 
   return 1; 
 
