1f839cfe5c679b072820c0db5bac18ddc079a0a4
Bryan C. Bailey <bryanbailey@mac.com>
2017-11-18 16:29:33 +0100

    what data structure to use...

---
+++ catvsdog/catvsdog.java
@@ -0,0 +1,28 @@
+// Cat Vs. Dog: A Reality TV Show
+
+/* A program to compute the total number of satisfied viewers after each show
+ * based on the fact that each voter/viewer picks one dog/cat to stay and one to 
+ * throw out
+ */
+
+import java.util.*;
+
+public class catvsdog {
+
+    public static void main( String[] main ) {
+	
+	Scanner in = new Scanner(System.in);
+
+	int testCases = in.nextInt();
+	for( int i=0; i<testCases; i++ ) {
+	    
+	    int c = in.nextInt();
+	    int d = in.nextInt();
+	    int v = in.nextInt();
+	    
+	    for( int j=0; i<v; i++ ) {
+
+	    }
+	}
+    }
+}
